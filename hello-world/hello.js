9894a37ecd46a1dc10efcc1b59063915adb075c5
lizacouser <liza.couser.95@gmail.com>
2022-01-11 12:50:14 -0500

    hello-world project in express

---
+++ hello-world/hello.js
@@ -0,0 +1,76 @@
+const express = require("express");
+// Put this code after `const app = express()`
+
+const COUNTRY_DATA = [
+  {
+    path: "/english",
+    flag: "flag-of-United-States-of-America.png",
+    alt: "US Flag",
+    title: "Go to US English site",
+  },
+  {
+    path: "/french",
+    flag: "flag-of-France.png",
+    alt: "Drapeau de la france",
+    title: "Aller sur le site français",
+  },
+  {
+    path: "/serbian",
+    flag: "flag-of-Serbia.png",
+    alt: "Застава Србије",
+    title: "Идите на српски сајт",
+  },
+  {
+    path: "/spanish",
+    flag: "flag-of-Spain.png",
+    alt: "Bandera de España",
+    title: "Va al sitio Español",
+  },
+];
+
+const LANGUAGE_CODES = {
+  english: "en-US",
+  french: "fr-FR",
+  serbian: "sr-Cryl-rs",
+  spanish: "es-ES"
+};
+
+const morgan = require("morgan");
+const app = express();
+
+app.set("views", "./views");
+app.set("view engine", "pug");
+
+app.use(express.static("public"));
+app.use(morgan("common"));
+
+app.locals.currentPathClass = (path, currentPath) => {
+  return path === currentPath ? "current" : "";
+};
+
+app.get("/", (req, res) => {
+  res.redirect("/english");
+});
+
+app.get("/:language", (req, res, next) => {
+  const language = req.params.language;
+  const languageCode = LANGUAGE_CODES[language];
+  if (!languageCode) {
+    next(new Error(`Language not supported: ${language}`))
+  } else {
+    res.render(`hello-world-${language}`, {
+      countries: COUNTRY_DATA,
+      currentPath: req.path,
+      language: languageCode,
+    });
+  }
+})
+
+app.use((err, req, res, _next) => {
+  console.log(err);
+  res.status(404).send(err.message);
+});
+
+app.listen(3000, "localhost", () => {
+  console.log("Listening to port 3000.");
+});
