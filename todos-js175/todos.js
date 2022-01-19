c24ab426c248f1cf8a680b767429d552ac0351a9
lizacouser <liza.couser.95@gmail.com>
2022-01-19 14:30:48 -0500

    added flash messaging and validation to todos app

--- todos-js175/todos.js
+++ todos-js175/todos.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
 
 const app = express();
 const port = 3000;
@@ -16,6 +18,22 @@ app.use(express.static("public"));
 
 // tell Express about the format used by the form data
 app.use(express.urlencoded({ extended: false }));
+
+app.use(session({
+  name: "launch-school-todos-session-id",
+  resave: false,
+  saveUninitialized: true,
+  secret: "this is not very secure",
+}));
+
+app.use(flash());
+
+// Extract session info
+app.use((req, res, next) => {
+  res.locals.flash = req.session.flash;
+  delete req.session.flash;
+  next();
+});
 
 // return the list of todo lists sorted by completion status and title.
 const sortTodoLists = lists => {
@@ -63,8 +81,30 @@ app.get("/lists/new", (req, res) => {
 // post new list data
 app.post("/lists", (req, res) => {
   let title = req.body.todoListTitle.trim();
-  todoLists.push(new TodoList(title));
-  res.redirect("/lists");
+  if (title.length === 0) {
+    req.flash("error", "A title was not provided.");
+    res.render("new-list", {
+      flash: req.flash(),
+    });
+  } else if (title.length > 100) {
+    req.flash("error", "List title must be between 1 and 100 characters.");
+    req.flash("error", "This is another error.");
+    req.flash("error", "Here is still another error.");
+    res.render("new-list", {
+      flash: req.flash(),
+      todoListTitle: req.body.todoListTitle,
+    });
+  } else if (todoLists.some(list => list.title === title)) {
+    req.flash("error", "List title must be unique.");
+    res.render("new-list", {
+      flash: req.flash(),
+      todoListTitle: req.body.todoListTitle,
+    });
+  } else {
+    todoLists.push(new TodoList(title));
+    req.flash("success", "The todo list has been created.");
+    res.redirect("/lists");
+  }
 });
 
 app.listen(port, host, () => {
